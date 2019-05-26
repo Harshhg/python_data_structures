62bd37c77577665db16211362e97260271a9b719
Harsh Gupta <myfolder.pc@gmail.com>
2019-05-26 16:28:14 +0530

    Update DYNAMIC_PROGRAMMING_in_fibonacci.py

--- DYNAMIC_PROGRAMMING_in_fibonacci.py
+++ DYNAMIC_PROGRAMMING_in_fibonacci.py
@@ -1,15 +1,15 @@
 def fib(n,arr):
      if(arr[n] is not None):
-          return arr[n]         //fetching the computed value
+          return arr[n]         #fetching the computed value
      if(n==1 or n==2):
         return 1
      else:
-          arr[n] = fib(n-1,arr) + fib(n-2,arr)    //storing the computed value so that need not to compute again
+          arr[n] = fib(n-1,arr) + fib(n-2,arr)    #storing the computed value so that need not to compute again
 
      return arr[n]
 
 def fibarr(n):
-     arr = [None] * (n+1)     //initializing list with null values
+     arr = [None] * (n+1)     #initializing list with null values
      return fib(n,arr)
 
 print(fibarr(50))
