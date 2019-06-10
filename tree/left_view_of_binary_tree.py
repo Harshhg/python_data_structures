436e589e3789f73137bf1d7422b163163666bec6
Harsh Gupta <myfolder.pc@gmail.com>
2019-06-10 20:14:20 +0530

    Update left_view_of_binary_tree.py

--- tree/left_view_of_binary_tree.py
+++ tree/left_view_of_binary_tree.py
@@ -1,1 +1,18 @@
 #  Prints the Left view of the Binary Tree
+# Function printLeftView takes the root of the tree as input and print the left view of the binary tree
+
+def traversetree(root,ml,level):
+    if(level>ml):
+        print(root.data,end=" ")
+        ml=level
+    if(root.left is not None):
+        ml =visittree(root.left,ml,level+1)
+    if(root.right is not None):
+        ml = visittree(root.right,ml,level+1)
+    return ml
+    
+    
+    
+def printLeftView(root):
+    traversetree(root,0,1)
+   
